94f03a360700740036f888a15bb050e538a7834b
Anirudh <anirudhaas@gmail.com>
2024-11-20 18:38:43 +0530

    fourth

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import * as React from "react";
-import Box from "@mui/material/Box";  // Material UI for design.
-import InputLabel from "@mui/material/InputLabel"; 
-import MenuItem from "@mui/material/MenuItem";  
+import Box from "@mui/material/Box"; // Material UI for design.
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";  
+import Select from "@mui/material/Select";
 import { useState } from "react";
 import "./App.css"; // CSS styling
 
 export default function App() {
-
   // Fees Data JSON
   const feeData = {
     "Exam Fee": {
@@ -78,13 +77,11 @@ export default function App() {
   const [levelSelected, setLevelSelected] = useState("");
   const [amount, setAmount] = useState("");
 
-
   // ALL_COURSE Substituted with courses array
   const courses = ["Medical", "Dental", "Ayurveda"];
 
   // ALL_LEVEL Substituted with levels array
   const levels = ["UG", "PG", "DIPLOMA", "Ph.D"];
-
 
   // Input Handler for Fee select.
   const handleFeeChange = (event) => {
@@ -94,7 +91,7 @@ export default function App() {
     setLevelSelected("");
     setAmount("");
   };
-  
+
   // Input Handler for Nationality select.
   const handleNationalityChange = (event) => {
     setNationality(event.target.value);
@@ -103,14 +100,12 @@ export default function App() {
     setAmount(null); // Reset amount when nationality changes
   };
 
-
   // Input Handler for Course select.
   const handleCourseChange = (event) => {
     setCourseSelected(event.target.value);
     setLevelSelected(""); // Reset level when course changes
     setAmount(null); // Reset amount when course changes
   };
-
 
   // Input Handler for Level Select.
   const handleLevelChange = (event) => {
@@ -137,139 +132,146 @@ export default function App() {
   };
 
   return (
+    <Box className="container">
+      <img
+        id="image"
+        src="https://plus.unsplash.com/premium_photo-1714348661832-a0d4960c601e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="college_pic"
+      />
+      <h1 id="title">COURSES AT TROPOSHERE UNIVERSITY</h1>
+      <h3 id="formTitle">Application Form</h3>
+      {/* Fee Selection */}
+      <Box sx={{ width: 200, mt: 2 }}>
+        <FormControl fullWidth>
+          <InputLabel id="fee-select-label">Fee Type</InputLabel>
+          <Select
+            id="fee"
+            value={feeSelected}
+            label="Fee"
+            onChange={handleFeeChange}
+          >
+            {/* Render fee type */}
+            {Object.keys(feeData).map((item, index) => (
+              <MenuItem key={index} value={item}>
+                {item}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
 
-    
-      <Box className="container">
-        <img id="image" src="https://plus.unsplash.com/premium_photo-1714348661832-a0d4960c601e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="college_pic"/>
-        <h1 id="title">COURSES AT TROPOSHERE UNIVERSITY</h1>
-        <h3 id="formTitle">Application Form</h3>
-        {/* Fee Selection */}
-        <Box sx={{ width: 200, mt: 2 }}>
+      {/* Nationality Selection */}
+      <Box sx={{ width: 200, mt: 2 }}>
+        {feeSelected ? (
           <FormControl fullWidth>
-            <InputLabel id="fee-select-label">Fee Type</InputLabel>
+            <InputLabel id="nationality-select-label">Nationality</InputLabel>
             <Select
-              id="fee"
-              value={feeSelected}
-              label="Fee"
-              onChange={handleFeeChange}
+              id="nationality"
+              value={nationality}
+              label="Nationality"
+              onChange={handleNationalityChange}
             >
-              {Object.keys(feeData).map((item, index) => (
+              {/* Render nation data */}
+              {Object.keys(feeData[feeSelected]).map((item, index) => (
                 <MenuItem key={index} value={item}>
                   {item}
                 </MenuItem>
               ))}
             </Select>
           </FormControl>
-        </Box>
+        ) : (
+          //Conditionally Render Empty Data
 
-        {/* Nationality Selection */}
-        <Box sx={{ width: 200, mt: 2 }}>
-          {feeSelected ? (
-            <FormControl fullWidth>
-              <InputLabel id="nationality-select-label">Nationality</InputLabel>
-              <Select
-                id="nationality"
-                value={nationality}
-                label="Nationality"
-                onChange={handleNationalityChange}
-              >
-                {Object.keys(feeData[feeSelected]).map((item, index) => (
+          <FormControl fullWidth>
+            <InputLabel id="nationality-select-label">Nationality</InputLabel>
+            <Select value="" label="Select Nationality">
+              <MenuItem value="null"></MenuItem>
+            </Select>
+          </FormControl>
+        )}
+      </Box>
+
+      {/* Course Selection */}
+      <Box sx={{ width: 200, mt: 2 }}>
+        {feeSelected && nationality ? (
+          <FormControl fullWidth>
+            <InputLabel id="course-select-label">Course</InputLabel>
+            <Select
+              id="course"
+              value={courseSelected}
+              label="Course"
+              onChange={handleCourseChange}
+            >
+              {/* Check if 'ALL_COURSES​' is available in the selected nationality */}
+              {feeData[feeSelected][nationality]["ALL_COURSES"] ? (
+                // If 'ALL_COURSES​' exists, render static courses like Medical, Dental, Ayurveda
+                courses.map((item, index) => (
                   <MenuItem key={index} value={item}>
                     {item}
                   </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          ) : (
-            <FormControl fullWidth>
-              <InputLabel id="nationality-select-label">Nationality</InputLabel>
-              <Select value="" label="Select Nationality">
-                <MenuItem value="null"></MenuItem>
-              </Select>
-            </FormControl>
-          )}
-        </Box>
-
-        {/* Course Selection */}
-        <Box sx={{ width: 200, mt: 2 }}>
-          {feeSelected && nationality ? (
-            <FormControl fullWidth>
-              <InputLabel id="course-select-label">Course</InputLabel>
-              <Select
-                id="course"
-                value={courseSelected}
-                label="Course"
-                onChange={handleCourseChange}
-              >
-                {/* Check if 'ALL_COURSES​' is available in the selected nationality */}
-                {feeData[feeSelected][nationality]["ALL_COURSES"] ? (
-                  // If 'ALL_COURSES​' exists, render static courses like Medical, Dental, Ayurveda
-                  courses.map((item, index) => (
-                    <MenuItem key={index} value={item}>
-                      {item}
-                    </MenuItem>
-                  ))
-                ) : (
-                  <MenuItem value="">No courses exist</MenuItem>
-                )}
-              </Select>
-            </FormControl>
-          ) : (
-            <FormControl fullWidth>
+                ))
+              ) : (
+                <MenuItem value="">No courses exist</MenuItem>
+              )}
+            </Select>
+          </FormControl>
+        ) : (
+          //Conditionally Render Empty Data
+          <FormControl fullWidth>
             <InputLabel id="nationality-select-label">Courses</InputLabel>
             <Select value="" label="Select Nationality">
               <MenuItem value="null"></MenuItem>
             </Select>
           </FormControl>
-          )}
-        </Box>
-          
+        )}
+      </Box>
 
-        {/* Levels Selection */}
-        <Box sx={{ width: 200, mt: 2}}>
-          {feeSelected && nationality && courseSelected ? (
-            <FormControl fullWidth>
-              <InputLabel id="course-select-label">Level</InputLabel>
-              <Select
-                id="level"
-                value={levelSelected}
-                label="Levels"
-                onChange={handleLevelChange}
-              >
-                {/* Check if 'ALL_LEVEL​' is available in the selected nationality */}
-                {feeData[feeSelected][nationality]["ALL_COURSES"]["ALL_LEVEL"]
-                  ? // If 'ALL_COURSES​ exists, render levels (UG, PG, etc.)
-                    levels.map((item, index) => (
-                      <MenuItem key={index} value={item}>
-                        {item}
-                      </MenuItem>
-                    ))
-                  : // If 'ALL_LEVEL' doesn't exist, render available levels or courses
-                    Object.keys(
-                      feeData[feeSelected][nationality]["ALL_COURSES"]
-                    ).map((item, index) => (
-                      <MenuItem key={index} value={item}>
-                        {item}
-                      </MenuItem>
-                    ))}
-              </Select>
-            </FormControl>
-          ) : (<FormControl fullWidth>
+      {/* Levels Selection */}
+      <Box sx={{ width: 200, mt: 2 }}>
+        {feeSelected && nationality && courseSelected ? (
+          <FormControl fullWidth>
+            <InputLabel id="course-select-label">Level</InputLabel>
+            <Select
+              id="level"
+              value={levelSelected}
+              label="Levels"
+              onChange={handleLevelChange}
+            >
+              {/* Check if 'ALL_LEVEL​' is available in the selected nationality */}
+              {feeData[feeSelected][nationality]["ALL_COURSES"]["ALL_LEVEL"]
+                ? // If 'ALL_COURSES​ exists, render levels (UG, PG, etc.)
+                  levels.map((item, index) => (
+                    <MenuItem key={index} value={item}>
+                      {item}
+                    </MenuItem>
+                  ))
+                : // If 'ALL_LEVEL' doesn't exist, render available levels or courses
+                  Object.keys(
+                    feeData[feeSelected][nationality]["ALL_COURSES"]
+                  ).map((item, index) => (
+                    <MenuItem key={index} value={item}>
+                      {item}
+                    </MenuItem>
+                  ))}
+            </Select>
+          </FormControl>
+        ) : (
+          //Conditionally Render Empty Data
+
+          <FormControl fullWidth>
             <InputLabel id="nationality-select-label">Level</InputLabel>
             <Select value="" label="Select Nationality">
               <MenuItem value="null"></MenuItem>
             </Select>
-          </FormControl>)}
-
-  
-        </Box>
-        {/* Display the amount */}
-          {amount && (
-            <Box sx={{mt:2 }}>
-              <h1 id="amount">Amount: {amount}</h1>
-            </Box>
-          )}
+          </FormControl>
+        )}
       </Box>
- 
+      {/* Display the amount */}
+      {amount && (
+        <Box sx={{ mt: 2 }}>
+          <h1 id="amount">Amount: {amount}</h1>
+        </Box>
+      )}
+    </Box>
   );
 }
