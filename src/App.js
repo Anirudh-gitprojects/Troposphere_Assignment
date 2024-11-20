8de5e40905b20a38a297085782f0ff782dffabf1
Anirudh <anirudhaas@gmail.com>
2024-11-20 17:37:02 +0530

    fourth

--- src/App.js
+++ src/App.js
@@ -140,7 +140,6 @@ export default function App() {
 
     
       <Box className="container">
-     
         <img id="image" src="https://plus.unsplash.com/premium_photo-1714348661832-a0d4960c601e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="college_pic"/>
         
         <h1 id="title">COURSES AT TROPOSHERE UNIVERSITY</h1>
